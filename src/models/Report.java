da2380f997d8b51c2a27065d73337f8edc1d5a18
Nashimoto <na4moto@gmail.com>
2021-02-21 14:33:49 +0900

    Add Report class

---
+++ src/models/Report.java
@@ -0,0 +1,116 @@
+package models;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.Lob;
+import javax.persistence.ManyToOne;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+
+
+@Table(name = "report")
+@NamedQueries({
+    @NamedQuery(
+            name = "getAllReports",
+            query = "SELECT r FROM Report AS r ORDER BY r.id DESC"
+            ),
+
+    @NamedQuery(
+            name = "getReportCount",
+            query = "SELECT COUNT(r) FROM Report AS r"
+            ),
+})
+@Entity
+public class Report {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @ManyToOne
+    @JoinColumn(name = "employee_id", nullable = false)
+    private Employee employee;
+
+    @Column(name = "report_date", nullable = false)
+    private Date report_date;
+
+    @Column(name = "title", length = 255, nullable = false)
+    private String title;
+
+    @Lob
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Column(name = "create_time", nullable = false)
+    private Timestamp create_time;
+
+    @Column(name = "update_time", nullable = false)
+    private Timestamp update_time;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public Employee getEmployee() {
+        return employee;
+    }
+
+    public void setEmployee(Employee employee) {
+        this.employee = employee;
+    }
+
+    public Date getReport_date() {
+        return report_date;
+    }
+
+    public void setReport_date(Date report_date) {
+        this.report_date = report_date;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Timestamp getCreate_time() {
+        return create_time;
+    }
+
+    public void setCreate_time(Timestamp create_time) {
+        this.create_time = create_time;
+    }
+
+    public Timestamp getUpdate_time() {
+        return update_time;
+    }
+
+    public void setUpdate_time(Timestamp update_time) {
+        this.update_time = update_time;
+    }
+
+
+
+}
